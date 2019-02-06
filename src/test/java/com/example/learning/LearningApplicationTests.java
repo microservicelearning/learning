7336a15ee343a019fd2d5a194eb25eab9a3289bc
vikrant0406f5 <vikrshar@in.ibm.com>
2019-02-06 17:39:50 +0530

    my first commit

---
+++ src/test/java/com/example/learning/LearningApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.learning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
